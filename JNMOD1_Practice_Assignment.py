30d5657d62120e28662cc71b2b40440094eae30d
JoseL2G <jgarc147@asu.edu>
2022-08-04 16:57:23 -0700

    added commit

--- JNMOD1_Practice_Assignment.py
+++ JNMOD1_Practice_Assignment.py
@@ -1,4 +1,4 @@
-# 1. Accept the user's first and last name and print them in 
+# 1) Accept the user's first and last name and print them in 
 #   reverse order with a space between them
 first_name = input("Input your First Name: ")
 last_name = input("Input your Last Name: ")
@@ -6,7 +6,7 @@ last_name = input("Input your Last Name: ")
 print("\nHello {} {}".format(last_name, first_name))
 
 
-# 2. Accept an integer (n) and compute the value of n+nn+nnn
+# 2) Accept an integer (n) and compute the value of n+nn+nnn
 n_number = str(input("Input an Integer: "))
 nn_number = n_number * 2
 nnn_number = n_number * 3
@@ -14,14 +14,14 @@ total_n_value = int(n_number) + int(nn_number) + int(nnn_number)
 print(total_n_value)
 
 
-# 3. Ask the user "What country are you from?" then print the following
+# 3) Ask the user "What country are you from?" then print the following
 # statement: "I have heard that [input] is a beautiful country"
 user_country = input("What country are you from?: ")
 
 print("\nI have heard that {} is a beautiful country!".format(user_country))
 
 
-# 4. What is the output of the following Python code
+# 4) What is the output of the following Python code
 x = 10
 y = 50
 if (x ** 2 > 100 and y < 100):
@@ -30,7 +30,7 @@ if (x ** 2 > 100 and y < 100):
     10 50'''
 
 
-# 5. What is the output of the following addition (+) operator, and why does
+# 5) What is the output of the following addition (+) operator, and why does
 # code chunk execute this way?
 a = [10, 20]
 b = a
@@ -51,12 +51,22 @@ print(b)
 # address, so both show the same data when printed out
 
 
-# 6. what is the output of the following code and what arithmetic operators is being used here?
+# 6) what is the output of the following code and what arithmetic operators is being used here?
 print(2%6)
 # Answer: 2, this is because the % operand basically returns the remainder of the quotient of 2/6, 
 # but since 6 cannot be divided into 2 any whole number of times, the remainder would be 2
 
 
-# 7. What is the output of the following code and what arithmetic operators are used here?
+# 7) What is the output of the following code and what arithmetic operators are used here?
 print(2 * 3 ** 3 * 4)
-# The result 
+# The result is 216 and the multiplication and exponential operands are being used
+
+
+# 8) What is a text editor
+
+
+# 9) What is Python?
+
+
+# 10)What is jupyter notebook, what type of python environment is it, and what alternatives
+# are there to jupyter notebook?
