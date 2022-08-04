89f071323f7ac658f1e10837a89ed1bbae53dd7a
Joseluis Garcia <jgarc145@asu.edu>
2022-08-04 13:37:21 -0700

    new Commit

--- JNMOD1_Practice_Assignment.py
+++ JNMOD1_Practice_Assignment.py
@@ -1,3 +1,13 @@
-#test edit
-def main():
-    print('Hello World')
+# 1. Accept the user's first and last name and print them in 
+#   reverse order with a space between them
+
+first_name = input("Input your First Name:")
+last_name = input("Input your Last Name: ")
+
+print("\nHello {} {}".format(last_name, first_name))
+
+
+# 2. Accept an integer (n) and compute the value of n+nn+nnn
+n_number = input("Enter a number")
+
+
