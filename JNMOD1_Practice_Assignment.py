2c93b8f0dd65480592a360bae4da406a175f9f70
JoseL2G <jgarc147@asu.edu>
2022-08-04 16:56:13 -0700

    All coding problems solved

--- JNMOD1_Practice_Assignment.py
+++ JNMOD1_Practice_Assignment.py
@@ -8,13 +8,9 @@ print("\nHello {} {}".format(last_name, first_name))
 
 # 2. Accept an integer (n) and compute the value of n+nn+nnn
 n_number = str(input("Input an Integer: "))
-
 nn_number = n_number * 2
-
 nnn_number = n_number * 3
-
 total_n_value = int(n_number) + int(nn_number) + int(nnn_number)
-
 print(total_n_value)
 
 
@@ -22,5 +18,45 @@ print(total_n_value)
 # statement: "I have heard that [input] is a beautiful country"
 user_country = input("What country are you from?: ")
 
-print("I have heard that {} is a beautiful country!".format(user_country))
+print("\nI have heard that {} is a beautiful country!".format(user_country))
 
+
+# 4. What is the output of the following Python code
+x = 10
+y = 50
+if (x ** 2 > 100 and y < 100):
+    print(x, y)
+'''Result:
+    10 50'''
+
+
+# 5. What is the output of the following addition (+) operator, and why does
+# code chunk execute this way?
+a = [10, 20]
+b = a
+b += [30, 40]
+print(a)
+print(b)
+
+'''Result:
+>>> print(a)
+[10, 20, 30, 40]
+>>> print(b)
+[10, 20, 30, 40]
+'''
+# The (+) operand in this code section appends the list of data to the existing 
+# array of data. The reason this section of code runs this way is because in python,
+# variables are more like pointers to a specific memory address, so when
+# b appends more data, the address is changed and a is pointing to the
+# address, so both show the same data when printed out
+
+
+# 6. what is the output of the following code and what arithmetic operators is being used here?
+print(2%6)
+# Answer: 2, this is because the % operand basically returns the remainder of the quotient of 2/6, 
+# but since 6 cannot be divided into 2 any whole number of times, the remainder would be 2
+
+
+# 7. What is the output of the following code and what arithmetic operators are used here?
+print(2 * 3 ** 3 * 4)
+# The result 
