fe9d055813a8ab9c4e4513c44ed966a264bc6be3
JoseL2G <73457118+JoseL2G@users.noreply.github.com>
2022-08-27 13:34:39 -0700

    new edit

--- funcTest.py
+++ funcTest.py
@@ -1,1 +1,6 @@
-print("hello world")
+def exercise2_function():
+    acceptableNums, acceptableSpecChars = [str(i) for i in range(10)], ['$', '#', '@']
+    userPassword = input("Input your password: ")
+
+    while len(userPassword) < 6 or len(userPassword) > 16:
+        print("Password is too short or too long")
