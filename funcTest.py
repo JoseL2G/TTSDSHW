7464c00e4887b720cfc0fc3870a206f31a8085a5
JoseL2G <jgarc147@asu.edu>
2022-09-11 12:08:04 -0700

    New edits for HW

--- funcTest.py
+++ funcTest.py
@@ -1,3 +1,2 @@
-#The year can be evenly divided by 4, is a leap year, unless:
-#The year can be evenly divided by 100, it is NOT a leap year, unless:
-#The year is also evenly divisible by 400. Then it is a leap year.
+for i in range(5):
+    print(i)
