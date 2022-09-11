03a67891c2067bfbfc98b664d703ee7a57d600d9
JoseL2G <jgarc147@asu.edu>
2022-09-11 13:51:37 -0700

    new edits and itertools ex completed

--- funcTest.py
+++ funcTest.py
@@ -1,2 +1,19 @@
-for i in range(5):
-    print(i)
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import permutations
+
+
+sample_input = 'HACK 2'
+edited_input = sample_input.split()
+
+def input_permutations(input_list):
+    sorted_list = [*input_list[0]]
+    sorted_list.sort()
+    
+    list_permutations = list(permutations(sorted_list, int(input_list[1])))
+    
+    for i in list_permutations:
+        print('')
+        for j in range(int(input_list[1])):
+            print(i[j],end='')
+
+input_permutations(edited_input)
